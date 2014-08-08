e1e3639c663f5d03885ffb8f22fa12fa5830006f
quan zhou <ext.zhou@gmail.com>
2014-08-08 15:28:37 +0000

    123

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,9 @@
 function start(){
-
+  console.log("Request handler 'start' was called.");
+  return "Hello start";
 }
 
 function upload(){
+  console.log("Request handler 'Upload' was called.");
+  return "Hello upload";
 }
